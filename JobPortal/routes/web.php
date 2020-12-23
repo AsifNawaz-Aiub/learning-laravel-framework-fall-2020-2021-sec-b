313126ae2cb13c7fa5c6dfac441781d6406e76eb
Asif Nawaz <asifnawaz5926@gmail.com>
2020-12-23 13:35:00 +0600

    Completed CRUD in Admin

---
+++ JobPortal/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login.index');
+});
+
+Route::get('/login', 'loginController@index');
+Route::post('/login', 'loginController@verify');
+
+Route::get('/register', 'adminController@register');
+Route::post('/register', 'adminController@registerPost');
+
+Route::get('/logout', 'logoutController@index');
+
+Route::group(['middleware'=>['sess']], function(){
+ Route::get('/admin', 'adminController@index');
+ Route::get('/admin/list', 'adminController@emplist')->name('emp.list');
+ Route::get('/details/{id}', 'adminController@show')->name('emp.show');
+ Route::get('/edit/{id}', 'adminController@edit')->name('emp.edit');
+ Route::post('/edit/{id}', 'adminController@update');
+ Route::get('/delete/{id}', 'adminController@delete');
+ Route::post('/delete/{id}', 'adminController@destroy');
+
+});
